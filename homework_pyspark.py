3cdf8c40de994da2f754039cc06ad93014e47c13
ax1sis <40387302+ax1sis@users.noreply.github.com>
2022-05-06 12:30:42 +0300

    Update homework_pyspark.py

--- homework_pyspark.py
+++ homework_pyspark.py
@@ -14,29 +14,31 @@ from pyspark.sql.types import IntegerType
 
 
 def main(crimes = unknown[0],offense_codes = unknown[1],output = unknown[2]):
+    # Initiate spark
     spark = SparkSession.builder              .appName("testing")              .enableHiveSupport()              .getOrCreate()
+    
+    # Read files
     df = (spark.read.format("csv").options(header="true")
         .load(crimes))
-
     offense_codes_df = (spark.read.format("csv").options(header="true")
         .load(offense_codes))
-
+    
+    # Cleaning and getting rid of duplicates
     offense_codes_df = offense_codes_df.drop_duplicates(['code'])
-
+    df = df.dropna(subset = ['DISTRICT'])
     df = df.withColumn("OFFENSE_CODE", df["OFFENSE_CODE"].cast(IntegerType()))
     offense_codes_df = offense_codes_df.withColumn("CODE", offense_codes_df["CODE"].cast(IntegerType()))
 
     df = df.join(offense_codes_df,df.OFFENSE_CODE == offense_codes_df.CODE, how = 'left')
-    df = df.dropna(subset = ['DISTRICT'])
+
+    # Median monthly Crimes
     df=df.select(concat(df.YEAR,df.MONTH)
                   .alias("year_month"),"*")
-
-
     median_monthly_crimes = df         .groupBy(df.DISTRICT, df.year_month)         .agg(count(df.INCIDENT_NUMBER))         .groupBy('DISTRICT')         .agg(percentile_approx('count(INCIDENT_NUMBER)', 0.5).alias("crimes_monthly"))
-
+    
     df = df.withColumn('first_part_name', split(df.NAME, ' - ').getItem(0))
     temp = df.groupBy('DISTRICT', 'first_part_name').agg(count('INCIDENT_NUMBER').alias('count'))
-
+    # Finding top3 names and concating them
     # define window
     w = Window().partitionBy('DISTRICT').orderBy(F.desc('count'))
 
@@ -49,13 +51,12 @@ def main(crimes = unknown[0],offense_codes = unknown[1],output = unknown[2]):
 
     temp_top_3 = first_highest.join(second_highest, on = 'DISTRICT', how = 'left')
     temp_top_3 = temp_top_3.join(third_highest, on = 'DISTRICT', how = 'left')
-
     temp_top_3 = temp_top_3.select(temp_top_3.DISTRICT,concat('first',lit(', '),'second',lit(', '),'third').alias('frequent_crime_types'))
 
+    #Creating a final datamart
+    
     final = df         .groupby(df.DISTRICT)         .agg(count(df.INCIDENT_NUMBER).alias('crimes_total'), avg(df.Lat).alias('lat'), avg(df.Long).alias('lng'))
-
     final = final.join(median_monthly_crimes, how = 'left', on = 'DISTRICT')
-
     final = final.join(temp_top_3, how = 'left', on = 'DISTRICT')
     final.write.parquet(output) 
 
